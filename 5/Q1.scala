8cdd868a627d757bb2bacf9ea7320352506e7d11
lakindukivin <140165115+lakindukivin@users.noreply.github.com>
2024-07-16 01:51:34 +0530

    Add files via upload

---
+++ 5/Q1.scala
@@ -0,0 +1,31 @@
+import scala.io.StdIn.readLine;
+
+object Inventory extends App {
+
+  def getProductList: List[String] = {
+    def getProductHelper(acc: List[String]): List[String] = {
+      val products = readLine("Enter product / Enter 'done' to finish: ");
+      if (products.toLowerCase == "done") {
+        acc;
+      } else {
+        getProductHelper(acc :+ products);
+      }
+    }
+    getProductHelper(Nil);
+  }
+
+  def printProductList(productList: List[String]): Unit = {
+    productList.zipWithIndex.foreach { case (productList, index) =>
+      println(s"${index + 1}) $productList");
+    }
+  }
+
+  def getTotalProducts(productList: List[String]): Int = {
+    productList.length;
+  }
+
+  val products = getProductList;
+  println("Products: ");
+  printProductList(products);
+  println(s"Total number of products: ${getTotalProducts(products)}");
+}
